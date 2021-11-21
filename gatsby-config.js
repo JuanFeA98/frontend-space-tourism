4a557b1b11713c8dd756abfbeec3f921d9f73682
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-11-21 08:06:38 -0500

    Organizando carpetas y archivos

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,19 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Space Tourism",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        display: "standalone",
+        icon: "src/assets/favicon-32x32.png",
+        crossOrigin: `use-credentials`,
+      },
+    }
+  ],
 };
