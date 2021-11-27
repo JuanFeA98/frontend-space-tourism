f4eac768309920bd95041ec58273b265e8c19304
JuanFe <55063644+JuanFeA98@users.noreply.github.com>
2021-11-27 18:37:31 -0500

    First commit

--- src/pages/destination.js
+++ src/pages/destination.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-import Header from '../components/header'
+import Header from '../components/Header.js'
 
 import '../styles/destination.css'
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { Link } from "gatsby"
 
 import '../styles/index.css'
 import Header from '../components/Header.js'
@@ -9,6 +8,22 @@ const IndexPage = () => {
     <main className="home">
       <title>Home Page</title>
       <Header/>
+      <div className="home__content">
+        <p>
+          So, you want to travel to
+        </p>
+        <h1>SPACE</h1>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely go to 
+          outer space and not hover kind of on the edge of it. Well sit back, and relax 
+          because we’ll give you a truly out of this world experience!
+        </p>
+        <div className="home__explore">
+          <h2>
+            EXPLORE
+          </h2>
+        </div>
+      </div>
     </main>
   )
 }
